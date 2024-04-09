d3ad02c0c6b670c927bb87ba0151c8c5c08add04
Cezar Nogueira <cezartv1221@gmail.com>
2024-04-09 20:21:32 -0300

    Editar e modificar estrutura geral do form

--- src/assets/Form/formulario.jsx
+++ src/assets/Form/formulario.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Formulario_module.css";
 
-function Formulario () {
+function Formulario() {
+
+    const [cep, setCep] = useState('');
+    const [endereco, setEndereco] = useState(null);
+
+    const buscaCep = async () => {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const data = await response.json();
+        setEndereco(data);
+    };
+
+    const [whatsapp, setWhatsapp] = useState('');
+
+    const apiWhatsapp = async () => {
+    const link = "https://api.whatsapp.com/send?phone=000000000&text=sua%20mensagem"
+
+    window.open(link);
+    };
+
     return (
-        <form>
-            <div className="data-box">
-            <p>Nome Completo</p>
-            <input type="text"/>
+        <div className="wrapper__form">
+            <div className="form-box">
+                <form>
+                    <div className="data-box">
+                        <p>Nome Completo</p>
+                        <input type="text" />
+                    </div>
+                    <div className="data-box">
+                        <p>Telefone</p>
+                        <input type="text" />
+                    </div>
+                    <div className="data-box">
+                        <p>E-mail</p>
+                        <input type="text" />
+                    </div>
+                    <div className="data-box">
+                        <p>Endereço</p>
+                        <div className="ender-button-box">
+                            <button className="ender-button">Buscar</button>
+                        </div>
+                    </div>
+                    <button className="send-button" onClick={apiWhatsapp}>Enviar</button>
+                </form>
             </div>
-            <div className="data-box">
-            <p>Telefone</p>
-            <input type="text"/>
-            </div>
-            <div className="data-box">
-            <p>E-mail</p>
-            <input type="text"/>
-            </div>
-            <div className="data-box">
-            <p>Endereço</p>
-            <input type="text"/>
-            </div>
-        </form>
+        </div>
     );
 };
 
